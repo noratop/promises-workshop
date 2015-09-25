266f58a96133ed8e40dd6801ba623d49554bbea7
Nora Top <top.nora@gmail.com>
2015-09-25 19:28:30 +0000

    removed unecessary variable declaration and add check on synonym existence

--- exercise-5.js
+++ exercise-5.js
@@ -4,7 +4,7 @@ var colors = require('colors');
 
 var APIKey = '814d0e373e7fe92fe71b94abdddd015b';
 
-var synonymPromises = request('http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=5&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5')
+request('http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=5&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5')
 .spread(function(result,body){
     return JSON.parse(body);
 }).map(function(item, index, arrayLength) {
@@ -19,7 +19,7 @@ var synonymPromises = request('http://api.wordnik.com:80/v4/words.json/randomWor
         }
     });
 }).then(function(response) {
-    console.log(response);
+    //console.log(response);
    
     response.forEach(function(elt){
         console.log()
@@ -27,25 +27,25 @@ var synonymPromises = request('http://api.wordnik.com:80/v4/words.json/randomWor
             console.log('Synonyms found for the word: '+elt.word);
             if (elt.response.adverb){
                 console.log('As an adverb:'.bold.gray);
-                elt.response.adverb.syn.forEach(function(elt){
+                if (elt.response.adverb.syn) elt.response.adverb.syn.forEach(function(elt){
                     console.log('- '.bold.magenta+elt.magenta);
                 });
             }
             if (elt.response.adjective){
                 console.log('As an adjective:'.bold.gray);
-                elt.response.adjective.syn.forEach(function(elt){
+                if (elt.response.adjective.syn) elt.response.adjective.syn.forEach(function(elt){
                     console.log('- '.bold.magenta+elt.magenta);
                 });
             }
             if (elt.response.noun){
                 console.log('As a noun:'.bold.gray);
-                elt.response.noun.syn.forEach(function(elt){
+                if (elt.response.noun.syn) elt.response.noun.syn.forEach(function(elt){
                     console.log('- '.bold.magenta+elt.magenta);
                 });
             }
             if (elt.response.verb){
                 console.log('As a verb:'.bold.gray);
-                elt.response.verb.syn.forEach(function(elt){
+                if (elt.response.verb.syn) elt.response.verb.syn.forEach(function(elt){
                     console.log('- '.bold.magenta+elt.magenta);
                 });   
             }
